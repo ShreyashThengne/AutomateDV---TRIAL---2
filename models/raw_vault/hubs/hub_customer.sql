988ddb1470efe30dfeb736628da53f628be31ec8
Thengne <2392871@cognizant.com>
2026-09-24 12:09:06 +0530

    preparing for testing

--- models/raw_vault/hubs/hub_customer.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='incremental', schema='DEMO', tags=['daily_batch', 'hub']) }}
-
-{%- set source_model = "v_stg_orders" -%}
-{%- set src_pk = "CUSTOMER_PK" -%}
-{%- set src_nk = "CUSTOMERKEY" -%}
-{%- set src_ldts = "LOAD_DATE" -%}
-{%- set src_source = "RECORD_SOURCE" -%}
-
-{{ automate_dv.hub(src_pk=src_pk, src_nk=src_nk, src_ldts=src_ldts, src_source=src_source, source_model=source_model) }}
